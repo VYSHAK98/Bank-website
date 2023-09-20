2435108dc44f97041e35fd083a2b102679fd0f76
vyshak harikumar <vyshakharikumar98@gmail.com>
2023-09-20 14:19:15 +0530

    binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,22 +10,19 @@ export class LoginComponent implements OnInit{
   data="Happy Banking with us"
   data2="enter account number"
 
+  acno:string=""
+  psw:string=""
+
   constructor(){}
 
   ngOnInit(): void {
       
   }
   login(){
-    alert("login clicked")
-  }
-  acnoChange(event:any){
-
-    console.log(event.target.value);
+    console.log(this.acno);
+    console.log(this.psw);
     
-  }
-
-  pswChange(event:any){
-    console.log(event.target.value);
+    
     
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  constructor(private rout:Router){}
+
   ngOnInit(): void {
       
+  }
+  //methods
+  signup(){
+    // alert("signup worked")
+    this.rout.navigateByUrl("")
   }
 }
